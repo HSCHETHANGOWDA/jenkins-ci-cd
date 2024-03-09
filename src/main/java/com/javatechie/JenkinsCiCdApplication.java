fb55cfaa8d80ffd9a4865bf273ffe441796af235
chethan <chetangowda@gmail.com>
2024-03-09 15:27:45 +0530

    Pushed some changes to check jenkins

--- src/main/java/com/javatechie/JenkinsCiCdApplication.java
+++ src/main/java/com/javatechie/JenkinsCiCdApplication.java
@@ -10,17 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class JenkinsCiCdApplication {
 
+	@GetMapping("/greetings/{name}")
+	public String greetings(@PathVariable String name) {
+		name = name.split(" ")[0];
+		return "Hello " + name + " Congratulations you have successfully completed Jenkins CI/CD demo !";
+	}
 
-    @GetMapping("/greetings/{name}")
-    public String greetings(@PathVariable String name) {
-        //Basant kumar hota
-        //sarvindra kumar
-       name= name.split(" ")[0];
-        return "Hello " + name + " Congratulations you have successfully completed Jenkins CI/CD demo !";
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(JenkinsCiCdApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsCiCdApplication.class, args);
+	}
 
 }
